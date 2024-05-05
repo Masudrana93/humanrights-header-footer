0484efbd9e3f946704cb1abe8424a3515fa21c04
Masud Rana <masudrana730@gmail.com>
2024-05-05 14:01:54 +0600

    slider update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 
 $(window).load(function() {
     $('.flexslider').flexslider({
-      animation: "slide"
+      animation: "slide",
+      direction: "vertical",
+      slideshowSpeed:5000,
+      animationSpeed:7000,
+    
     });
   });
   
